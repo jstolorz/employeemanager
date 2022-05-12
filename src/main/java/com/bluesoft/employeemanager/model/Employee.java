2ab910d91ebade18d2e3d5c5fb225f7e503d0a64
Janusz <31537161+jstolorz@users.noreply.github.com>
2022-05-12 14:25:59 +0200

    Initial commit

---
+++ src/main/java/com/bluesoft/employeemanager/model/Employee.java
@@ -0,0 +1,15 @@
+package com.bluesoft.employeemanager.model;
+
+import java.io.Serializable;
+
+public class Employee implements Serializable {
+    private long id;
+    private String name;
+    private String email;
+    private String jobTitle;
+    private String phone;
+    private String imageURL;
+    private String employeeCode;
+
+
+}
