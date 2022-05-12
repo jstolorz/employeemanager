4e357af79016aafa5d3526b1a56d207c31662cbe
Janusz <31537161+jstolorz@users.noreply.github.com>
2022-05-12 15:03:11 +0200

    Adding Employee entity

--- src/main/java/com/bluesoft/employeemanager/model/Employee.java
+++ src/main/java/com/bluesoft/employeemanager/model/Employee.java
@@ -1,15 +1,101 @@
 package com.bluesoft.employeemanager.model;
 
+import javax.persistence.*;
 import java.io.Serializable;
 
+@Entity
 public class Employee implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(nullable = false, updatable = false)
     private long id;
     private String name;
     private String email;
     private String jobTitle;
     private String phone;
     private String imageURL;
+    @Column(nullable = false, updatable = false)
     private String employeeCode;
 
+    public Employee() {
+    }
 
+    public Employee(final String name, final String email, final String jobTitle, final String phone, final String imageURL, final String employeeCode) {
+        this.name = name;
+        this.email = email;
+        this.jobTitle = jobTitle;
+        this.phone = phone;
+        this.imageURL = imageURL;
+        this.employeeCode = employeeCode;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(final long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(final String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(final String email) {
+        this.email = email;
+    }
+
+    public String getJobTitle() {
+        return jobTitle;
+    }
+
+    public void setJobTitle(final String jobTitle) {
+        this.jobTitle = jobTitle;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(final String phone) {
+        this.phone = phone;
+    }
+
+    public String getImageURL() {
+        return imageURL;
+    }
+
+    public void setImageURL(final String imageURL) {
+        this.imageURL = imageURL;
+    }
+
+   public String getEmployeeCode() {
+        return employeeCode;
+    }
+
+    public void setEmployeeCode(final String employeeCode) {
+        this.employeeCode = employeeCode;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", jobTitle='" + jobTitle + '\'' +
+                ", phone='" + phone + '\'' +
+                ", imageURL='" + imageURL + '\'' +
+                ", employeeCode='" + employeeCode + '\'' +
+                '}';
+    }
 }
